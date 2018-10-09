c86559393a85221bd17480589596f2365802cd90
jameslee1187 <jameslee1187@gmail.com>
2018-10-09 22:46:59 +0000

    automatically backed up by learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,13 +3,14 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
+
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
-
+  response_hash["results"].each do |data|
+    if data["name"] == character
+      return data["films"]
+    end
+  end
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -23,6 +24,12 @@ def get_character_movies_from_api(character)
 end
 
 def print_movies(films_hash)
+  films_array.each do |film_link|
+  response_string = RestClient.get(film_link)
+  response_hash = JSON.parse(response_string)
+  return response_hash["title"]
+  
+  
   # some iteration magic and puts out the movies in a nice list
 end
 
